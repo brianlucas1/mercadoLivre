e70a0389bfe35f175030b7f3556aa757c277f8ff
Brian Lucas Kitani Camargo <blcamargo@stefanini.com>
2022-04-04 22:06:15 -0300

    Alteração no POM para gerar o JAR

--- src/main/java/br/com/mutante/MutanteApplication.java
+++ src/main/java/br/com/mutante/MutanteApplication.java
@@ -2,12 +2,20 @@ package br.com.mutante;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MutanteApplication {
 
+	
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MutanteApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MutanteApplication.class, args);
 	}
 
+	
+	
 }
