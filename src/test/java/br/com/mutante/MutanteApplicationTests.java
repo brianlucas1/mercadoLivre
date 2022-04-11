c4b921833740bf7e17c951b2f2be6bded7ba59f8
Brian Lucas Kitani Camargo <blcamargo@stefanini.com>
2022-04-11 11:07:24 -0300

    Incluido tests

--- src/test/java/br/com/mutante/MutanteApplicationTests.java
+++ src/test/java/br/com/mutante/MutanteApplicationTests.java
@@ -1,13 +1,62 @@
 package br.com.mutante;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.mutante.model.HumanoModel;
+import br.com.mutante.model.MutanteModel;
+import br.com.mutante.service.MutanteService;
+import junit.framework.TestCase;
 
 @SpringBootTest
 class MutanteApplicationTests {
+	
+	private String[] mutant =  new String[] { 
+			  "ATGCGA", 
+			  "CAGTGC", 
+			  "TTATGG", 
+			  "AGAAGG", 
+			  "CCCCTA", 
+			  "TCGCTG"};
+	
+	
+	private String[] hum1 =  new String[] { 
+			  "GTGCGA", 
+			  "CACTAC", 
+			  "TCCTGG", 
+			  "CGAAAG", 
+			  "CCCGTA", 
+			  "TCGCTG"};
+	
+	@InjectMocks
+	private MutanteService service;
 
 	@Test
-	void contextLoads() {
+	public void testeSucessMutant() throws Exception{
+		
+		String[] dna = mutant;
+		MutanteModel mutante = new MutanteModel(dna);
+		
+		boolean result = service.isMutant(dna);
+
+		// THEN the return is true
+		TestCase.assertTrue(result);
+		
+		
+	}
+	
+	@Test
+	public void testeErrorHuman() throws Exception{
+		
+		String[] dna = hum1;
+		HumanoModel h = new HumanoModel(dna);
+		
+		boolean result = service.isMutant(dna);
+
+		TestCase.assertFalse(result);
+		
 	}
 
 }
